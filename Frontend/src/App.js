e429166d900bf60390148d7e7f2dd3a75b23823c
Brian <blsvsanteliz@gmail.com>
2020-08-12 21:10:00 -0400

    setApi in state

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import About from "./components/About";
 import Home from "./components/Home";
 import AboutDinamic from "./components/AboutDinamic";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from './config/axios'
 
 function App() {
+  const [datos, setDatos] = useState([])
+  useEffect(() => {
+    const fetchData = async()=>{
+      try{
+        const response = await axios.get()
+        setDatos(response.data)
+      }catch{
+        throw new Error('ocurrio un error al obtener la data')
+      }
+    }
+    fetchData()
+  }, [])
   return (
     <Router>
       <Switch>
