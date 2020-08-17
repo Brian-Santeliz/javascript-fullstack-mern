00a00334e693bc8b6750e6b969492a2e50c57578
Brian <blsvsanteliz@gmail.com>
2020-08-17 17:34:53 -0400

    Add Component CItas

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from "react";
 import About from "./components/About";
-import Home from "./components/Home";
+import Cita from "./components/Cita";
 import AboutDinamic from "./components/AboutDinamic";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from './config/axios'
@@ -22,7 +22,7 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/about" component={()=><About/>} />
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={()=> <Cita datos={datos}/>} />
         <Route exact path="/about/:id" component={AboutDinamic} />
       </Switch>
     </Router>
---
+++ Frontend/src/components/Citas.js
@@ -0,0 +1,12 @@
+import React,{Fragment} from 'react';
+
+const Citas = () => {
+    return ( 
+        <Fragment>
+
+            <h1>Desde el compomente Home</h1>
+        </Fragment>
+     );
+}
+ 
+export default Citas;
