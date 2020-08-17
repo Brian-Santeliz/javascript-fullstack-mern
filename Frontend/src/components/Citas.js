e0834485233db5a5b1ac9ef5d0952c24c68facc4
Brian <blsvsanteliz@gmail.com>
2020-08-17 19:23:08 -0400

    Show Citas in the Frontend

--- Frontend/src/components/Citas.js
+++ Frontend/src/components/Citas.js
@@ -6,14 +6,23 @@ const Citas = ({datos}) => {
     if(datos.length === 0) return null
     return ( 
         <Fragment>
-            <h1 className="">Administraodor de Citas</h1>
-                <div className="container mt-5 py-5">
-                    <div className="row">
-                      <div className="col-md-12 d-flex-justify-content-center">
-                          <a href="#" className="btn btn-success text-uppercase font-weight-bold">Crar cita</a>
-                      </div>
+            <h1 className="my-5">Administrador de Citas</h1>
+             <div className="container">
+                <div className="row">
+                    <div className="col-md-12 d-flex justify-content-center">
+                        <a href="#" className="btn btn-success text-uppercase font-weight-bold px-5 py-2">Crear cita</a>
+                    </div>
+                    <div className="col-md-8">
+                        <div className="list-group">
+                            {datos.map(dato=>(
+                                <h3 className="mb-4">
+                                    {dato.nombre}
+                                </h3>
+                            ))}
+                        </div>
                     </div>
                 </div>
+             </div>
             
         </Fragment>
      );
