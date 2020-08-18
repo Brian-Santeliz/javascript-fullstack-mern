9521de38b7f456b3a61c84b3f1eb9c26ceec9fb1
Brian <blsvsanteliz@gmail.com>
2020-08-17 20:07:26 -0400

    Style to the citas

--- Frontend/src/components/Citas.js
+++ Frontend/src/components/Citas.js
@@ -1,31 +1,48 @@
-import React,{Fragment} from 'react';
+import React, { Fragment } from "react";
 
-const Citas = ({datos}) => {
+const Citas = ({ datos }) => {
+  //when datos is equal a array empty, don't render this component
+  if (datos.length === 0) return null;
+  return (
+    <Fragment>
+      <h1 className="my-5">Administrador de Citas</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 d-flex justify-content-center">
+            <a
+              href="#"
+              className="btn btn-success text-uppercase font-weight-bold px-5 py-2 mb-5"
+            >
+              Crear cita
+            </a>
+          </div>
+          <div className="col-md-8 mx-auto mb-5">
+            <div className="list-group">
+              {datos.map((dato) => (
+                <a
+                  href=""
+                  className="list-group-item list-group-item-action  p-5"
+                  key={dato._id}
+                >
+                  <div className="d-flex  justify-content-between mb-4">
+                    <h3 className="mb-4">{dato.nombre}</h3>
+                    <span className="fecha-alta">
+                      {dato.fecha} - {dato.hora}
+                    </span>
+                  </div>
+              <p className="mb-0">{dato.sintomas}</p>
+              <div className="contacto py-3">
+              <p>Dueño: {dato.propietario}</p>
+              <p>Telefono: {dato.telefono}</p>
+              </div>
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
-    //when datos is equal a array empty, don't render this component
-    if(datos.length === 0) return null
-    return ( 
-        <Fragment>
-            <h1 className="my-5">Administrador de Citas</h1>
-             <div className="container">
-                <div className="row">
-                    <div className="col-md-12 d-flex justify-content-center">
-                        <a href="#" className="btn btn-success text-uppercase font-weight-bold px-5 py-2">Crear cita</a>
-                    </div>
-                    <div className="col-md-8">
-                        <div className="list-group">
-                            {datos.map(dato=>(
-                                <h3 className="mb-4">
-                                    {dato.nombre}
-                                </h3>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-             </div>
-            
-        </Fragment>
-     );
-}
- 
 export default Citas;
